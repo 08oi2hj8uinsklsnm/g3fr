275c53e6fe9dd3458a89d625647f2c25d1a14b4e
08oi2hj8uinsklsnm <43113226+08oi2hj8uinsklsnm@users.noreply.github.com>
2018-09-11 19:03:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ client.on('message', async msg => {
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 
-					.setFooter("Masters Bot")
+					.setFooter("Smile Life Bot")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					// eslint-disable-next-line max-depth
